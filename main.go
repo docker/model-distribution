872408fc674301b89068d34ed2a082c7683e9985
Ignasi <ignasi.lopez.luna@gmail.com>
2025-02-21 11:58:27 +0100

    Adds OCI Image Index

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 
+	"github.com/google/go-containerregistry/pkg/v1/static"
+
 	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	v1 "github.com/google/go-containerregistry/pkg/v1"
@@ -14,7 +16,6 @@ import (
 	"github.com/google/go-containerregistry/pkg/v1/remote"
 	"github.com/google/go-containerregistry/pkg/v1/types"
 
-	"github.com/docker/model-distribution/pkg/layer"
 	"github.com/docker/model-distribution/pkg/utils"
 )
 
@@ -30,71 +31,84 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("1. Creating reference for target image...")
-	ref, err := name.ParseReference(*tag)
-	if err != nil {
-		log.Fatalf("parsing reference: %v", err)
-	}
-	fmt.Printf("   Reference: %s\n", ref.String())
+	layer := createLayer(source)
+	armImage := createPlatformImage("arm", "darwin", layer)
+	amdImage := createPlatformImage("amd", "darwin", layer)
+	idx := createImageIndex([]v1.Image{armImage, amdImage})
+	ref := pushImage(tag, idx)
 
-	fmt.Printf("2. Reading from source: %s\n", *source)
+	fmt.Printf("\nSuccessfully pushed multi-arch image %s\n", ref.String())
+}
+
+func createLayer(source *string) v1.Layer {
+	fmt.Printf("Reading from source: %s\n", *source)
 	fileContent, err := utils.ReadContent(*source)
 	if err != nil {
 		log.Fatalf("reading content: %v", err)
 	}
 	fmt.Printf("   Size: %s\n", utils.FormatBytes(len(fileContent)))
 
-	fmt.Println("3. Creating imgLayer from file content...")
-	l := layer.New(fileContent)
-	layerSize, _ := l.Size()
+	fmt.Println("Creating imgLayer from file content...")
+	layer := static.NewLayer(fileContent, "application/vnd.docker.ai.model.file.v1+gguf")
+	layerSize, _ := layer.Size()
 	fmt.Printf("   Layer size: %s\n", utils.FormatBytes(int(layerSize)))
+	return layer
+}
 
-	fmt.Println("4. Creating empty image with artifact configuration...")
-	img := empty.Image
-
+// createPlatformImage creates a single platform image with the given layer
+func createPlatformImage(arch, os string, l v1.Layer) v1.Image {
+	platformImg := empty.Image
 	configFile := &v1.ConfigFile{
-		Architecture: "unknown",
-		OS:           "unknown",
+		Architecture: arch,
+		OS:           os,
 		Config:       v1.Config{},
 	}
-
-	img, err = mutate.ConfigFile(img, configFile)
+	platformImg, err := mutate.ConfigFile(platformImg, configFile)
 	if err != nil {
-		log.Fatalf("setting config: %v", err)
+		log.Fatalf("setting config for %s/%s: %v", arch, os, err)
 	}
 
-	// Set up artifact manifest according to OCI spec
-	img = mutate.MediaType(img, types.OCIManifestSchema1)
-	img = mutate.ConfigMediaType(img, "application/vnd.docker.ai.model.config.v1+json")
+	platformImg = mutate.MediaType(platformImg, types.OCIManifestSchema1)
+	platformImg = mutate.ConfigMediaType(platformImg, "application/vnd.docker.ai.model.manifest.v1+json")
 
-	fmt.Println("5. Appending imgLayer to image...")
-	img, err = mutate.AppendLayers(img, l)
+	fmt.Printf("Appending layer to image for %s/%s...\n", arch, os)
+	platformImg, err = mutate.AppendLayers(platformImg, l)
 	if err != nil {
-		log.Fatalf("appending imgLayer: %v", err)
+		log.Fatalf("appending layer: %v", err)
 	}
 
-	fmt.Println("6. Getting manifest details...")
-	manifest, err := img.Manifest()
-	if err != nil {
-		log.Fatalf("getting manifest: %v", err)
+	return platformImg
+}
+
+// createImageIndex creates an OCI image index with given images
+func createImageIndex(images []v1.Image) v1.ImageIndex {
+	fmt.Println("Creating image index...")
+
+	var addenda []mutate.IndexAddendum
+	for _, img := range images {
+		cf, err := img.ConfigFile()
+		if err != nil {
+			log.Fatalf("getting config file: %v", err)
+		}
+		addenda = append(addenda, mutate.IndexAddendum{
+			Add: img,
+			Descriptor: v1.Descriptor{
+				Platform: &v1.Platform{
+					Architecture: cf.Architecture,
+					OS:           cf.OS,
+					//TODO add features
+				},
+				ArtifactType: "application/vnd.docker.ai.model.config.v1+json",
+			},
+		})
 	}
 
-	fmt.Println("\nManifest details:")
-	fmt.Printf("  MediaType: %s\n", manifest.MediaType)
-	fmt.Printf("  Config:")
-	fmt.Printf("    MediaType: %s\n", manifest.Config.MediaType)
-	fmt.Printf("    Size: %d bytes\n", manifest.Config.Size)
-	fmt.Printf("    Digest: %s\n", manifest.Config.Digest)
-	fmt.Printf("  Layers:\n")
-	for i, imgLayer := range manifest.Layers {
-		fmt.Printf("    Layer %d:\n", i+1)
-		fmt.Printf("      MediaType: %s\n", imgLayer.MediaType)
-		fmt.Printf("      Size: %d bytes\n", imgLayer.Size)
-		fmt.Printf("      Digest: %s\n", imgLayer.Digest)
-	}
-	fmt.Println()
+	idx := mutate.IndexMediaType(empty.Index, types.OCIImageIndex)
+	return mutate.AppendManifests(idx, addenda...)
+}
 
-	fmt.Println("7. Pushing image to registry...")
+func pushImage(tag *string, idx v1.ImageIndex) name.Reference {
+	fmt.Println("Pushing image index to registry...")
 	// Create progress channel
 	progressChan := make(chan v1.Update, 1)
 
@@ -108,15 +122,20 @@ func main() {
 	}()
 
 	// Show progress
-	go utils.ShowProgress("Uploading", progressChan64, -1) // -1 since total size might not be known
+	go utils.ShowProgress("Uploading", progressChan64, -1)
 
-	// Push the image with progress
-	if err := remote.Write(ref, img,
+	// Push the image index with progress
+	fmt.Println("Creating reference for target image...")
+	ref, err := name.ParseReference(*tag)
+	if err != nil {
+		log.Fatalf("parsing reference: %v", err)
+	}
+	fmt.Printf("   Reference: %s\n", ref.String())
+	if err := remote.WriteIndex(ref, idx,
 		remote.WithAuthFromKeychain(authn.DefaultKeychain),
 		remote.WithProgress(progressChan),
 	); err != nil {
-		log.Fatalf("writing image: %v", err)
+		log.Fatalf("writing image index: %v", err)
 	}
-
-	fmt.Printf("Successfully pushed %s\n", ref.String())
+	return ref
 }
